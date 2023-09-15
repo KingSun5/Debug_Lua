be212fd0eca58c3ea032989fd5eacb1f08e0f782
Sun <729916355@qq.com>
2023-09-15 14:10:08 +0800

    init

---
+++ lua_src/test.lua
@@ -0,0 +1,25 @@
+--test2
+
+print("111111111111111111111111")
+print("333333333333333333333333")
+
+local tt = {}
+
+tt[1] = "111"
+tt[2] = "222"
+
+print(#tt)
+
+-- function testA( ... )
+-- 	print(aaaa-1)
+-- end
+
+
+-- __G__TRACKBACK__ = function(msg)
+-- 	print(msg)
+-- end
+
+
+-- local status, msg = xpcall(testA, __G__TRACKBACK__)
+
+
